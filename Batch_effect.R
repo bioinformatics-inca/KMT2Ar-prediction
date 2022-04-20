dc244683413314bf0dd5a0dd43416c6c724bb612
Cristiane Esteves <64868534+tncristiane@users.noreply.github.com>
2022-04-20 12:02:53 -0300

    Add files via upload

---
+++ Batch_effect.R
@@ -0,0 +1,25 @@
+# load ML dataset
+load("Dataset_ML_KMT2Ar.RData")
+
+# log transformation of FPKM values
+df_final_matrix <- log10(df_final[,c(7:ncol(df_final))])
+
+# id patients in rownames
+rownames(df_final_matrix) <- df_final$patient
+
+# transpose matrix
+df_final_matrix <- t(df_final_matrix)
+
+# remove batch effect - by data study
+library(limma)
+df_final_matrix <- removeBatchEffect(df_final_matrix, batch = df_final$Study)
+
+# return to FPKM values
+df_final_matrix <- t(df_final_matrix)
+df_final_matrix <- 10^df_final_matrix
+
+# update values in initial dataset
+df_final[,c(7:ncol(df_final))] <- df_final_matrix
+
+# save ML dataset
+save(df_final, file = "Dataset_ML_KMT2Ar.RData")
