cb80ae8d6464534633d51d9ee69f9b71908a5bc9
Cristiane Esteves <64868534+tncristiane@users.noreply.github.com>
2022-04-19 21:16:57 -0300

    Add files via upload

---
+++ Analysis_correlation.R
@@ -0,0 +1,24 @@
+load("Dataset_ML_KMT2Ar.RData")
+
+table(df_final$Study)
+df_final$Study = NULL
+df_final$Age.years = NULL
+
+table(df_final$Leukemia)
+table(df_final$Age_group)
+
+head(df_final[,1:6])
+
+# CLEANING AND PRE-PROCESSING
+library(caret)
+
+
+#Remove highly correlated variables
+descrCor <-  cor(df_final[5:ncol(df_final)])
+highCorr <- sum(abs(descrCor[upper.tri(descrCor)]) > .999)
+highlyCorDescr <- findCorrelation(descrCor, cutoff = .90)
+df_leukemia <- df_final[,-highlyCorDescr]
+#14834 genes
+rm(descrCor,highCorr,highlyCorDescr)
+
+write.csv(df_leukemia, "sb_leuk.csv")
